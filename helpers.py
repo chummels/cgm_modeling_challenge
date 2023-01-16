4a38d6dc729c8081537841323701a0e79ebc4b01
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-01-16 15:48:41 -0800

    Major update for sample2 analysis; comparing with revised data Sameer sent.

---
+++ helpers.py
@@ -0,0 +1,5 @@
+import palettable
+
+# Colors to use throughout for revised vs original
+modeled_color = palettable.cartocolors.qualitative.Safe_10.mpl_colors[1]
+revised_color = palettable.cartocolors.qualitative.Safe_10.mpl_colors[0]
