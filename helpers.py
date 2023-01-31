20a5e3ba5c3c7981e9aac2198ca1f95ff1c436d7
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-01-30 17:45:45 -0800

    Significant progress towards an MLE summary plot.

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import matplotlib
 import palettable
 
 # Colors to use throughout for revised vs original
@@ -8,12 +9,37 @@ colors_for_variations = {
     'high-z': revised_color,
 }
 
+# For interpreting units
+property_given_key = {
+    'nH': 'Density',
+    'NHI': 'HI Column',
+    'vlos': 'LOS Velocity',
+    'l': 'Lengths',
+    'Z': 'Metallicity',
+    'pdf': 'PDF Value',
+    'T': 'Temperature',
+}
+key_given_property = {}
+for prop_key, prop in property_given_key.items():
+    key_given_property[prop] = prop_key
+logscale = {
+    'vlos': False,
+    'T': True,
+    'nH': True,
+    'Z': True,
+    'NHI': True,
+}
+logscale_props = []
+for prop_key, is_logscale in logscale.items():
+    if is_logscale:
+        logscale_props.append( prop_key )
+
 property_labels = {
     'vlos': r'$v_{\rm LOS}$ [km/s]',
-    'T': r'T [K]',
-    'nH': r'$n_{\rm H}$ [cm$^{-3}$]',
-    'Z': r'$Z$ [$Z_{\odot}$]',
-    'NHI': r'$N_{\rm H\,I}$ [cm$^{-2}$]',
+    'T': r'$\log T$ [K]',
+    'nH': r'$\log n_{\rm H}$ [cm$^{-3}$]',
+    'Z': r'$\log Z$ [$Z_{\odot}$]',
+    'NHI': r'$\log N_{\rm H\,I}$ [cm$^{-2}$]',
 }
 property_labels_no_units = {
     'vlos': r'$v_{\rm LOS}$',
@@ -65,17 +91,20 @@ autolims = {
     'Z': False,
     'NHI': False,
 }
-logscale = {
-    'vlos': False,
-    'T': True,
-    'nH': True,
-    'Z': True,
-    'NHI': True,
-}
 
-# Figure dimensions
-figure_width = 8. # Default figure width
-figure_height = figure_width / 2.
+# # Figure dimensions
+# figure_width = 3.376 # Default figure width in inches; MNRAS column width
+# # figure_width = 8. # Default figure width
+# figure_height = figure_width
+# # figure_height = figure_width / 2.
+# max_figure_height = 9.437 # Text height for MNRAS
+# large_fontsize = 14.4
+# normal_fontsize = 12
+# small_fontsize = 10.95
+# footnote_fontsize = 10
+lighter_textcolor = '0.4'
+lightest_textcolor = '0.6'
+# limits_whitespace = 0.05
 
 # Plot elements
 violin_width = 0.75 # In units of distance between violins
