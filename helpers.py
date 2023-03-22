70f29bfe26fe0154d8e5cf0eeae0b7e45e83245e
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-03-21 18:48:49 -0700

    Analysis update to understand the component structure of absorption.

--- helpers.py
+++ helpers.py
@@ -18,6 +18,7 @@ property_given_key = {
     'Z': 'Metallicity',
     'pdf': 'PDF Value',
     'T': 'Temperature',
+    'x': 'LOS Position',
 }
 key_given_property = {}
 for prop_key, prop in property_given_key.items():
@@ -28,6 +29,7 @@ logscale = {
     'nH': True,
     'Z': True,
     'NHI': True,
+    'x': False,
 }
 logscale_props = []
 for prop_key, is_logscale in logscale.items():
@@ -40,6 +42,7 @@ property_labels = {
     'nH': r'$\log n_{\rm H}$ [cm$^{-3}$]',
     'Z': r'$\log Z$ [$Z_{\odot}$]',
     'NHI': r'$\log N_{\rm H\,I}$ [cm$^{-2}$]',
+    'x': r'$x$ [kpc]',
 }
 property_labels_no_units = {
     'vlos': r'$v_{\rm LOS}$',
@@ -47,6 +50,7 @@ property_labels_no_units = {
     'nH': r'$n_{\rm H}$',
     'Z': r'$Z$',
     'NHI': r'$N_{\rm H\,I}$',
+    'x': r'$x$',
 }
 property_labels_1D = {
     'vlos': r'$\frac{ d N_{\rm H\,I} }{d v_{\rm LOS}}$',
@@ -54,6 +58,7 @@ property_labels_1D = {
     'nH': r'$\frac{ d N_{\rm H\,I} }{d \log n_{\rm H}}$',
     'Z': r'$\frac{ d N_{\rm H\,I} }{d \log Z}$',
     'NHI': r'$\frac{ d N_{\rm H\,I} }{d \log N_{\rm H\,I}}$',
+    'x': r'$\frac{ d N_{\rm H\,I} }{dx}$',
 }
 correlation_coefficient_property_labels = {}
 for key, item in property_labels.items():
@@ -76,6 +81,7 @@ lims = {
     'nH': [ 1e-7, 100 ],
     'Z': [ 1e-3, 30 ],
     'NHI': [ 1e9, 1e17 ],
+    'x': [ -15, 15 ],
 }
 lims_1D = {
     'vlos': [ 3e9, 1e19 ],
@@ -83,6 +89,15 @@ lims_1D = {
     'nH': [ 1e12, 1e20 ],
     'Z': [ 1e12, 1e20 ],
     'NHI': [ 1e12, 3e20 ],
+    'x': [ 3e9, 1e19 ],
+}
+lims_zoomed = {
+    'vlos': [ -70, 70 ],
+    'T': [ 10**2.5, 2.5e6 ],
+    'nH': [ 10**-5, 10 ],
+    'Z': [ 10**-2.5, 10**0.75 ],
+    'NHI': [ 1e9, 1e17 ],
+    'x': [ -15, 15 ],
 }
 autolims = {
     'vlos': False,
@@ -90,6 +105,7 @@ autolims = {
     'nH': False,
     'Z': False,
     'NHI': False,
+    'x': False,
 }
 
 # # Figure dimensions
@@ -111,3 +127,26 @@ violin_width = 0.75 # In units of distance between violins
 
 def percentile_str_fn( fraction ):
     return '{:.1f}'.format( fraction * 100 )
+
+identifiers_to_slnums = {
+    'r1_0.6000_0.6000_0.2500_r2_0.6000_0.6000_0.7500': '00',
+    'r1_0.8000_0.4000_0.2500_r2_0.8000_0.4000_0.7500': '01',
+    'r1_0.2000_0.6000_0.2500_r2_0.2000_0.6000_0.7500': '02',
+    'r1_0.8000_0.5000_0.2500_r2_0.8000_0.5000_0.7500': '03',
+    'r1_0.4000_0.6000_0.2500_r2_0.4000_0.6000_0.7500': '04',
+    'r1_0.8000_0.4500_0.2500_r2_0.8000_0.4500_0.7500': '05',
+    'r1_0.8000_0.5500_0.2500_r2_0.8000_0.5500_0.7500': '06',
+    'r1_0.4000_0.4500_0.2500_r2_0.4000_0.4500_0.7500': '07',
+    'r1_0.4000_0.5500_0.2500_r2_0.4000_0.5500_0.7500': '08',
+    'r1_0.8000_0.6000_0.2500_r2_0.8000_0.6000_0.7500': '09',
+    'r1_0.2000_0.4000_0.2500_r2_0.2000_0.4000_0.7500': '10',
+    'r1_0.2000_0.5000_0.2500_r2_0.2000_0.5000_0.7500': '11',
+    'r1_0.6000_0.5000_0.2500_r2_0.6000_0.5000_0.7500': '12',
+    'r1_0.6000_0.4000_0.2500_r2_0.6000_0.4000_0.7500': '13',
+    'r1_0.6000_0.5500_0.2500_r2_0.6000_0.5500_0.7500': '14',
+    'r1_0.6000_0.4500_0.2500_r2_0.6000_0.4500_0.7500': '15',
+    'r1_0.2000_0.4500_0.2500_r2_0.2000_0.4500_0.7500': '16',
+    'r1_0.2000_0.5500_0.2500_r2_0.2000_0.5500_0.7500': '17',
+    'r1_0.4000_0.4000_0.2500_r2_0.4000_0.4000_0.7500': '18',
+    'r1_0.4000_0.5000_0.2500_r2_0.4000_0.5000_0.7500': '19'
+}
